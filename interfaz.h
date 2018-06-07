ffbdf5debcda90faabbef87800dcfae9f0a14843
equipazo <esmeg17@gmail.com>
2018-06-06 23:40:22 -0500

    proyecto

---
+++ interfaz.h
@@ -0,0 +1,30 @@
+#ifndef INTERFAZ_H
+#define INTERFAZ_H
+#include<QFileDialog>
+#include<QLabel>
+#include <QMainWindow>
+
+namespace Ui {
+class interfaz;
+}
+
+class interfaz : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    interfaz(QWidget *parent = 0);
+    ~interfaz();
+/*protected:
+    void changeEvent(QEvent *e);*/
+private:
+    Ui::interfaz *ui;
+
+private slots:
+    void on_pushButton_clicked();
+
+
+    void on_pushButton_2_clicked();
+};
+
+#endif // INTERFAZ_H
